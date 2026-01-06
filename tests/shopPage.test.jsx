f6623cd7ef0799b1c81bbc80f16acee0f2c64d5f
joshp423 <205880399+joshp423@users.noreply.github.com>
2026-01-06 21:48:40 +1300

    finished productTile test

--- tests/shopPage.test.jsx
+++ tests/shopPage.test.jsx
@@ -1,5 +1,6 @@
 import { describe, test, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
+import { useOutletContext } from 'react-router-dom';
 
 //mock what comes back from react router dom by mocking the whole thing
 vi.mock('react-router-dom', async () => {
@@ -12,13 +13,31 @@ vi.mock('react-router-dom', async () => {
 
 import ShopPage from '../src/components/Shop-page/shopPage';
 
-//test that product tiles are displaying accurately - amount
-describe('productPage', () => {
+//test that product tiles are displaying accurately
+describe('ShopPage', () => {
     test('dom is rendering products correctly', () => {
 
-        render(<ShopPage />);
+      const mockProducts = [
+        { id: 1, title: 'Product 1', image: 'img1.jpg', rating: {rate: 5, count:250} },
+        { id: 2, title: 'Product 2', image: 'img2.jpg', rating: {rate: 5, count:250} },
+      ];
 
-        const tiles = screen.getElementByTestID('product-tile');
-        expect(tiles).toHaveLength(2);
+      const mockSetProducts = vi.fn();
+      const mockSetCart = vi.fn();
+      const mockCart = new Map();
+
+      useOutletContext.mockReturnValue({
+        products: mockProducts,
+        setProducts: mockSetProducts,
+        cart: mockCart,
+        setCart: mockSetCart
+      });
+
+      render(<ShopPage />);
+
+      const tiles = screen.getAllByTestId('product-tile');
+
+      expect(tiles).toHaveLength(2);
+
     })
 })
